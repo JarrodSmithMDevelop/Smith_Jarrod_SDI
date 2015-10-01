e43a26fcc761fbfd719e5641ed3954232b22503a
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-01 17:38:41 -0500

    Added all kids
    All 3 kids have been added into code.

--- js/script.js
+++ js/script.js
@@ -13,8 +13,15 @@ var favSport = "Baseball"                   //Sons favorite sport           Stri
 
 // Output
 
+// Printing information about Shelby
 console.log ("I have " + numKids + " kids and they are awesome! But I would like to tell you some about them. \n" + kids[0] +
     " was my first born she is now " + age[0] + ". She likes to play video games and her favorite game is " + favGame + ".")
+
+// Printing information about Jodeah
 console.log ("My second child is " + kids[1] + ", he just turned " + age[1] + " back in June. He really thinks he is big now. \n " +
-    " It is " + favColor + " that blue is Jodeah\'s favorite color." )
+    "It is " + favColor + " that blue is Jodeah\'s favorite color." )
+
+// Printing information about Elijah
+console.log ("My youngest son is " + kids[2] + ", he just turned " + age[2] + ". He is also the only child in the house that's favorite \n " +
+    "sport is " + favSport + ".")
 
