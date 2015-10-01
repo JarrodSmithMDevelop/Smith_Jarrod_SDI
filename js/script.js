e7de24bd95d333230dc55488065df54dc51d331c
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-01 16:09:14 -0500

    Variables set
    I have set all the variables.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@
 
 // Variables
 
-
+var kids = ["Shelby", "Jodeah", "Elijah"]   //My kids names     Array of strings
+var age = [12, 7, 5]                        //My kids ages      Array of integers
+var favGame = "Minecraft"                   //My daughters favorite game    String variable
+var favColor = true                         //Sons favorite color is blue   Boolean variable
+var favSport = "Baseball"                   //Sons favorite sport           String variable
 
 // Output
