8faeefae54a23ed5bb26cf8ea4ea11a6f6eec607
Jarrod Smith <jasmith2@fullsail.edu>
2015-10-01 16:23:24 -0500

    Daughter added
    I have added my daughter into the code.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 
 // Variables
 
+var numKids = 3                             //Number of kids    int variable
 var kids = ["Shelby", "Jodeah", "Elijah"]   //My kids names     Array of strings
 var age = [12, 7, 5]                        //My kids ages      Array of integers
 var favGame = "Minecraft"                   //My daughters favorite game    String variable
@@ -11,3 +12,7 @@ var favColor = true                         //Sons favorite color is blue   Bool
 var favSport = "Baseball"                   //Sons favorite sport           String variable
 
 // Output
+
+console.log ("I have " + numKids + " they are awesome! But I would like to tell you some about them. \n" + kids[0] +
+    " was my first born she is now " + age[0] + ". She likes to play video games and her favorite game is " + favGame + ".")
+
